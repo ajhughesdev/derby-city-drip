47d98fbab1f123f13210220da4560422b001fbea
Andrew J Hughes <andrew@ajhughes.dev>
2021-11-01 17:30:01 -0400

    drop down nav, little css details

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("nav").style.marginTop = "-250px";
+  document.getElementById("top-nav").style.height = "250px";
+  document.getElementById("main").style.marginTop = "250px";
+}
+
+function closeNav() {
+  document.getElementById("nav").style.marginTop = "0"
+  document.getElementById("top-nav").style.height = "0";
+  document.getElementById("main").style.marginTop = "0";
+}
