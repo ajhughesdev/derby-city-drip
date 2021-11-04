674ba2ab4e1c92670f157b0f12029d4fc7c15707
Andrew J Hughes <andrew@ajhughes.dev>
2021-11-04 05:24:02 -0400

    begin tablet stylesheet

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,12 @@
-function openNav() {
-  document.getElementById("top-nav").style.height = "250px";
-  document.getElementById("mobile-nav-push-down").style.paddingTop = "250px";
-}
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+if (viewportWidth <= 767) {
+  function openNav() {
+    document.getElementById("top-nav").style.height = "250px";
+    document.getElementById("mobile-nav-push-down").style.paddingTop = "250px";
+  };
 
-function closeNav() {
-  document.getElementById("top-nav").style.height = "0";
-  document.getElementById("mobile-nav-push-down").style.paddingTop = "0";
-}
+  function closeNav() {
+    document.getElementById("top-nav").style.height = "0";
+    document.getElementById("mobile-nav-push-down").style.paddingTop = "0";
+  };
+};
