9111a104a4dabdf3e2439e86b2f4c94ba6402e65
Andrew J Hughes <andrew@ajhughes.dev>
2021-11-19 10:56:26 -0500

    implementing dynamic button text

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,31 @@
 var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 if (viewportWidth <= 767) {
+  var x = document.getElementById("top-nav");
+  var y = document.getElementById("mobile-nav-push-down");
+
   function openNav() {
-    document.getElementById("top-nav").style.height = "250px";
-    document.getElementById("mobile-nav-push-down").style.paddingTop = "250px";
+    x.style.height = "250px";
+    y.style.paddingTop = "250px";
   };
 
   function closeNav() {
-    document.getElementById("top-nav").style.height = "0";
-    document.getElementById("mobile-nav-push-down").style.paddingTop = "0";
+    x.style.height = "0";
+    y.style.paddingTop = "0";
   };
 };
+
+
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+if (viewportWidth >= 767) {
+  var z = document.getElementById("hero-btn");
+  z.innerText = "view menu";
+  z.style.marginTop = "2rem";
+  z.style.paddingTop = "1rem";
+  z.style.paddingBottom = "1rem";
+  z.style.paddingLeft = "3.5rem";
+  z.style.paddingRight = "3.5rem";
+}
+
 
 function emailSignup() {
   if (document.getElementById("name").validity.valueMissing || document.getElementById("email").validity.valueMissing) {
